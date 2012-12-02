7f6aea3c6d3c4a9b3630cf4b5ed45586031fb38a
Johannes Utzig <jutzig.dev@googlemail.com>
2012-12-02 22:46:06 +0100

    implemented greeter

--- greeter/src/main/java/org/example/greeter/impl/GreeterService.java
+++ greeter/src/main/java/org/example/greeter/impl/GreeterService.java
@@ -3,10 +3,16 @@ package org.example.greeter.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.apache.felix.scr.annotations.ReferencePolicy;
 import org.example.greeter.service.Greeter;
 
+@Component
 public class GreeterService {
 
+	@Reference(referenceInterface=Greeter.class,bind="addGreeter",unbind="removeGreeter",cardinality=ReferenceCardinality.MANDATORY_MULTIPLE,policy=ReferencePolicy.DYNAMIC)
 	private List<Greeter> greeters = new ArrayList<Greeter>();
 	
 	public void listGreetings()
@@ -14,5 +20,14 @@ public class GreeterService {
 		for (Greeter greeter : greeters) {
 			System.out.println(greeter.greet());
 		}
+	}
+	
+	protected void addGreeter(Greeter greeter){
+		System.out.println("Adding greeter "+greeter);
+		System.out.println(greeter.greet());
+	}
+	
+	protected void removeGreeter(Greeter greeter) {
+		System.out.println("Removing greeter "+greeter);
 	}
 }
--- helloworld/src/main/java/org/example/HelloWord.java
+++ helloworld/src/main/java/org/example/HelloWord.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.example.greeter.service.Greeter;
+
 /**
  * I see javadoc
  * @author utzig
  *
  */
-public class HelloWord {
+@Component
+@Service
+public class HelloWord implements Greeter{
 
 	public String sayHello()
 	{
 		return "Hello World";
+	}
+
+	public String greet() {
+		return sayHello();
 	}
 
 }
