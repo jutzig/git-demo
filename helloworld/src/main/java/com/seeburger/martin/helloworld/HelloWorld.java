c8e4192e6f782ef945f898d9611f6dcc534b9039
Martin Hristov <m.hristov@seeburger.com>
2012-12-04 10:57:21 +0200

    Added HelloWorld main class + JUnit tests

---
+++ helloworld/src/main/java/com/seeburger/martin/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.seeburger.martin.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println(sayHello());
+	}
+
+	public static String sayHello()
+	{
+		return "Hello, OKMONOG!";
+	}
+
+}
---
+++ helloworld/src/test/java/com/seeburger/martin/helloworld/TestHelloWorld.java
@@ -0,0 +1,22 @@
+package com.seeburger.martin.helloworld;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+
+public class TestHelloWorld extends TestCase
+{
+
+	@Test
+	public void testSayHello()
+	{
+		fail("This should fail!");
+	}
+
+	@Test
+	public void testSayHello2()
+	{
+		assertEquals("Hello, OKMONOG!", HelloWorld.sayHello());
+	}
+}
