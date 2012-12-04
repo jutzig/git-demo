8b514a7cd7f148d4908334f63d8d9b4ffc39db88
Ivellina <Ivellina@Ivelina>
2012-12-04 10:55:30 +0200

    test

---
+++ helloworld/src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * HelloWorld.java
+ *
+ * created at Dec 4, 2012 by Ivellina <YOURMAILADDRESS>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+
+public class HelloWorld
+{
+    public static String sayHello() {
+        return  "Hello";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(sayHello());
+    }
+}
+
+
+
---
+++ helloworld/src/test/java/TestHelloWorld.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * TestHelloWorld.java
+ *
+ * created at Dec 4, 2012 by Ivellina <YOURMAILADDRESS>
+ *
+ * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ */
+
+public class TestHelloWorld
+{
+
+    private HelloWorld helloWorld = new HelloWorld();
+
+
+    @Test
+    public void test()
+    {
+        fail();
+    }
+
+}
+
+
+
